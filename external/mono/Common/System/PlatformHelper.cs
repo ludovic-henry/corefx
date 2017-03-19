d9b9134580e00e4cbab42b34fbeea896caaa6f98
Ludovic Henry <ludovic@xamarin.com>
2017-03-19 16:21:04 -0400

    Add System.Diagnostics.Debug and System.Private.Uri to System.csproj

---
+++ external/mono/Common/System/PlatformHelper.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System
+{
+	internal static partial class PlatformHelper
+	{
+		public static readonly bool IsUnix = Environment.OSVersion.Platform == PlatformID.Unix;
+		public static readonly bool IsWindows = Environment.OSVersion.Platform != PlatformID.Unix;
+	}
+}
