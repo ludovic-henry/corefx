669552ac0c740d4349074fd6e928f6a0ed0db87a
Ludovic Henry <luhenry@microsoft.com>
2018-04-27 17:19:16 -0400

    [mono] Move out unnecessary function declaration from header file

--- src/Native/Unix/System.Native/pal_networkchange.h
+++ src/Native/Unix/System.Native/pal_networkchange.h
@@ -24,6 +24,5 @@ enum class NetworkChangeKind : int32_t
 typedef void (*NetworkChangeEvent)(int32_t sock, NetworkChangeKind notificationKind);
 
 DLLEXPORT void SystemNative_ReadEvents(int32_t sock, NetworkChangeEvent onNetworkChange);
-NetworkChangeKind ReadNewLinkMessage(nlmsghdr* hdr);
 
 END_EXTERN_C
