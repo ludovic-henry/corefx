d86cf9d76770661da0b512c5312a4055d48b5100
Ludovic Henry <ludovic@xamarin.com>
2018-04-27 23:35:50 -0400

    [mono] Enable -Wmissing-prototypes warning

---
+++ src/Native/Unix/System.Native/pal_random.h
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma once
+
+#include "pal_compiler.h"
+
+BEGIN_EXTERN_C
+
+#include "pal_types.h"
+
+/*
+Shims CCRandomGenerateBytes, putting the resulting CCRNGStatus value in pkCCStatus.
+
+Returns 1 on success, 0 on system error (see pkCCStatus), -1 on input error.
+*/
+void SystemNative_GetNonCryptographicallySecureRandomBytes(uint8_t* buffer, int32_t bufferLength);
+
+END_EXTERN_C
